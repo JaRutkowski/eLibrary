2bc1eac882df705c621f9b8e3b0e1cc472fc0c9b
rutko <rutko@192.168.0.122>
2018-03-03 17:40:58 +0100

    JaRutkowski Raising the technology stack
    Java 8 -> Java 9
    Hibernate 5.2.10.Final -> 5.2.13.Final

--- source/src/hibernate/java/com/javafee/hibernate/dao/HibernateUtil.java
+++ source/src/hibernate/java/com/javafee/hibernate/dao/HibernateUtil.java
@@ -6,20 +6,36 @@ import java.util.logging.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory;
+	private static final ServiceRegistry serviceRegistry;
 	private static final Session session;
 
 	static {
-		try {
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-			session = sessionFactory.openSession();
-		} catch (HibernateException e) {
-			Logger.getLogger("app").log(Level.WARNING, e.getMessage());
-			throw new ExceptionInInitializerError(e);
-		}
+	       try
+	        {
+	    	    Configuration configuration = new Configuration().configure();
+	            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+	            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	            session = sessionFactory.openSession();
+
+	        }
+	        catch (HibernateException he)
+	        {
+	            System.err.println("Error creating Session: " + he);
+	            throw new ExceptionInInitializerError(he);
+	        }
+		// try {
+		// sessionFactory = new Configuration().configure().buildSessionFactory();
+		// session = sessionFactory.openSession();
+		// } catch (HibernateException e) {
+		// Logger.getLogger("app").log(Level.WARNING, e.getMessage());
+		// throw new ExceptionInInitializerError(e);
+		// }
 	}
 
 	public static SessionFactory getSessionFactory() {
