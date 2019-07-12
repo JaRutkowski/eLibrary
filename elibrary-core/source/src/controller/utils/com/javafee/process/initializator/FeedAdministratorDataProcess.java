7f319fb8b1849f13cfcff2f22fec06ee042d6833
Jakub Rutkowski <Jakub.Rutkowski@sapiens.com>
2019-07-12 14:24:41 +0200

    JaRutkowski #31 Settings - part 1.10 - working commit

--- elibrary-core/source/src/controller/utils/com/javafee/process/initializator/FeedAdministratorDataProcess.java
+++ elibrary-core/source/src/controller/utils/com/javafee/process/initializator/FeedAdministratorDataProcess.java
@@ -3,10 +3,12 @@ package com.javafee.process.initializator;
 import com.javafee.hibernate.dao.HibernateUtil;
 import com.javafee.hibernate.dao.common.Common;
 import com.javafee.hibernate.dao.common.Constants;
+import com.javafee.hibernate.dao.common.Query;
 import com.javafee.process.Process;
+import lombok.extern.java.Log;
 
+@Log
 public class FeedAdministratorDataProcess implements Process {
-
     @Override
     public void execute() {
         feedAdministratorData();
@@ -16,12 +18,13 @@ public class FeedAdministratorDataProcess implements Process {
         if (!Common.findUserDataById(Constants.DATA_BASE_ADMIN_ID).isPresent()) {
             HibernateUtil.createAndGetEntityManager();
             HibernateUtil.getEntityManager().getTransaction().begin();
-            HibernateUtil.getEntityManager().createNativeQuery(
-                    "insert into public.com_user_data" +
-                            "(id_user_data, address, birth_date, document_number, e_mail, login, name, password, pesel, registered, sex, surname, id_city, id_system_properties)" +
-                            " values(0, null, null, null, null, \'GENERATED\', null, \'GENERATED\', null, null, null, null, null, null)")
+            HibernateUtil.getEntityManager().createNativeQuery(Query.ProcessQuery.FEED_ADMINISTRATOR_DATA.getValue())
+                    .setParameter(0, Constants.DATA_BASE_ADMIN_ID)
+                    .setParameter(1, Constants.DATA_BASE_ADMIN_LOGIN)
+                    .setParameter(2, Constants.DATA_BASE_ADMIN_PASSWORD)
                     .executeUpdate();
             HibernateUtil.getEntityManager().getTransaction().commit();
+            HibernateUtil.getEntityManager().close();
         }
     }
 }
